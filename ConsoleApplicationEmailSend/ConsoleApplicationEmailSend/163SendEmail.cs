1fb5629333dcaef44bd7f41bbca3e1e59b0b29f0
Wangshicheng <47958583+wangshichengwsc@users.noreply.github.com>
2020-04-30 16:06:07 +0800

    Add files via upload

---
+++ ConsoleApplicationEmailSend/ConsoleApplicationEmailSend/163SendEmail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationEmailSend
+{
+    public class SendEmail163
+    {
+
+        public void SendEmail()
+        {
+            // 设置发送方的邮件信息,例如使用网易的smtp
+            string smtpServer = "smtp.163.com"; //SMTP服务器
+            //string smtpServer = "smtp.aliyun.com"; //SMTP服务器
+            string mailFrom = "laowangwsc@163.com"; //登陆用户名
+            //string mailFrom = "zhanbozhiye@aliyun.com"; //登陆用户名
+            //string userPassword = "zbzy1111";//登陆密码
+            //string mailFrom = "scheng.wang@flowportal.com"; //登陆用户名
+            string userPassword = "Wsc123456";// 授权码
+
+            // 邮件服务设置
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;//指定电子邮件发送方式
+            smtpClient.Host = smtpServer; //指定SMTP服务器
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.Credentials = new System.Net.NetworkCredential(mailFrom, userPassword);//用户名和密码
+            smtpClient.Port = 25;
+            //string mailTo = "scheng.wang@flowportal.com";//收件人
+            string mailTo = "2569059551@qq.com";//收件人
+
+            // 发送邮件设置       
+            MailMessage mailMessage = new MailMessage(mailFrom, mailTo); // 发送人和收件人
+
+            mailMessage.Subject = "测试";//主题
+            mailMessage.Body = "测试展博邮件";//内容
+            mailMessage.BodyEncoding = Encoding.UTF8;//正文编码
+            mailMessage.IsBodyHtml = false;//设置为HTML格式
+            mailMessage.Priority = MailPriority.Low;//优先级
+
+            try
+            {
+                smtpClient.Send(mailMessage); // 发送邮件
+                Console.WriteLine("成功");
+            }
+            catch (SmtpException ex)
+            {
+                Console.WriteLine("失败：" + ex.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleApplicationEmailSend/ConsoleApplicationEmailSend/Class1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationEmailSend
+{
+    public class Class1
+    {
+
+        public void SendMailLocalhost()
+        {
+           
+            System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+            msg.To.Add("scheng.wang@flowportal.com");
+            //msg.To.Add("b@b.com");
+            /* msg.To.Add("b@b.com");  
+            * msg.To.Add("b@b.com");  
+            * msg.To.Add("b@b.com");可以发送给多人  
+            */
+            //msg.CC.Add(c@c.com);
+            /*  
+            * msg.CC.Add("c@c.com");  
+            * msg.CC.Add("c@c.com");可以抄送给多人  
+            */
+            msg.From = new MailAddress("2569059551@qq.com", "wangshicheng", System.Text.Encoding.UTF8);
+            /* 上面3个参数分别是发件人地址（可以随便写），发件人姓名，编码*/
+            msg.Subject = "这是测试邮件";//邮件标题  
+            msg.SubjectEncoding = System.Text.Encoding.UTF8;//邮件标题编码  
+            msg.Body = "邮件内容";//邮件内容  
+            msg.BodyEncoding = System.Text.Encoding.UTF8;//邮件内容编码  
+            msg.IsBodyHtml = false;//是否是HTML邮件  
+            msg.Priority = MailPriority.High;//邮件优先级 
+
+            SmtpClient client = new SmtpClient();
+            client.Host = "58.251.149.147";
+            object userState = msg;
+            try
+            {
+                client.SendAsync(msg, userState);
+                //简单一点儿可以client.Send(msg);  
+                ///MessageBox.Show("发送成功");
+                Console.WriteLine("发送成功");
+            }
+            catch (System.Net.Mail.SmtpException ex)
+            {
+                //MessageBox.Show(ex.Message, "发送邮件出错");
+                Console.WriteLine("发送邮件出错");
+            }
+        }
+
+        public void test()
+        {
+            var emailAcount = "2569059551@qq.com";
+                        var emailPassword = "wang19981002";
+                        var reciver = "scheng.wang@flowportal.com";
+                        var content = "测试";
+                        MailMessage message = new MailMessage();
+                        //设置发件人,发件人需要与设置的邮件发送服务器的邮箱一致
+            MailAddress fromAddr = new MailAddress("2569059551@qq.com");
+                     message.From = fromAddr;
+                     //设置收件人,可添加多个,添加方法与下面的一样
+         message.To.Add(reciver);
+                     //设置抄送人
+        // message.CC.Add("qwe123@163.com");
+                     //设置邮件标题
+         message.Subject = "Test";
+                     //设置邮件内容
+         message.Body = content;
+                     //设置邮件发送服务器,服务器根据你使用的邮箱而不同,可以到相应的 邮箱管理后台查看,下面是QQ的
+         SmtpClient client = new SmtpClient("smtp.qq.com", 25);
+                     //设置发送人的邮箱账号和密码
+         client.Credentials = new NetworkCredential(emailAcount, emailPassword);
+                     //启用ssl,也就是安全发送
+         client.EnableSsl = true;
+                     //发送邮件
+         client.Send(message);
+     }
+
+        public void SendMailUseZj()
+        {
+            System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+            msg.To.Add("scheng.wang@flowportal.com");
+            //msg.To.Add(b@b.com);
+            /*   
+            * msg.To.Add("b@b.com");   
+            * msg.To.Add("b@b.com");   
+            * msg.To.Add("b@b.com");可以发送给多人   
+            */
+            //msg.CC.Add("c@c.com");
+            /*   
+            * msg.CC.Add("c@c.com");   
+            * msg.CC.Add("c@c.com");可以抄送给多人   
+            */
+            msg.From = new MailAddress("2569059551@qq.com", "老王", System.Text.Encoding.UTF8);
+            /* 上面3个参数分别是发件人地址（可以随便写），发件人姓名，编码*/
+            msg.Subject = "这是测试邮件";//邮件标题    
+            msg.SubjectEncoding = System.Text.Encoding.UTF8;//邮件标题编码    
+            msg.Body = "邮件内容";//邮件内容    
+            msg.BodyEncoding = System.Text.Encoding.UTF8;//邮件内容编码    
+            msg.IsBodyHtml = false;//是否是HTML邮件    
+            msg.Priority = MailPriority.High;//邮件优先级    
+
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential("dulei@71info.com", "userpass");
+            //在71info.com注册的邮箱和密码    
+            //client.Host = "smtp.71info.com";
+            object userState = msg;
+            try
+            {
+                client.SendAsync(msg, userState);
+                //简单一点儿可以client.Send(msg);    
+               // MessageBox.Show("发送成功");
+            }
+            catch (System.Net.Mail.SmtpException ex)
+            {
+                //MessageBox.Show(ex.Message, "发送邮件出错");
+            }
+        }
+    }
+}
